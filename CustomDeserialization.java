f02e7d3f2628fb8ef252071884dcd5c194288007
Mahesh <33093725+MaheshMote12@users.noreply.github.com>
2019-01-16 23:43:37 +0530

    Create CustomDeserialization.java

---
+++ CustomDeserialization.java
@@ -0,0 +1,23 @@
+// deserialization
+// @JsonDeserialize(using = MultiplicationResultAttemptDeserializer.class)
+// add above annotation on model class level.
+
+public class MultiplicationResultAttemptDeserializer extends JsonDeserializer<MultiplicationResultAttempt> {
+              @Override
+              public MultiplicationResultAttempt deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
+              throws IOException, JsonProcessingException {
+              
+              
+              ObjectCodec oc = jsonParser.getCodec();
+              JsonNode node = oc.readTree(jsonParser);
+              
+              return new MultiplicationResultAttempt(node.
+              get("user").get("alias").asText(),
+              node.get("multiplication").get("factorA").asInt(),
+              node.get("multiplication").get("factorB").asInt(),
+              node.get("resultAttempt").asInt(),
+              node.get("correct").asBoolean());
+}
+}
+
+
